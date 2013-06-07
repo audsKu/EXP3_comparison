f510f852e3cbfd7062a740fb64cd37eacfa5c0aa
audrey.kuo <audrey.kuo@gmail.com>
2013-06-07 00:39:21 -0400

    Allow mass assigment of session id in Comparison.

--- app/models/comparison.rb
+++ app/models/comparison.rb
@@ -1,6 +1,6 @@
 class Comparison < ActiveRecord::Base
 
-  attr_accessible :original_id, :tester_response
+  attr_accessible :original_id, :tester_response, :session_id
   belongs_to :session
 
 end
