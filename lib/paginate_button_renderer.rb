1910232c79962c003ca04451b9743554b8f7a87e
audrey.kuo <audrey.kuo@gmail.com>
2013-06-08 12:07:45 -0400

    Update display of Comparison pages.

--- lib/paginate_button_renderer.rb
+++ lib/paginate_button_renderer.rb
@@ -3,7 +3,8 @@
 class PaginateButtonRenderer < WillPaginate::ActionView::LinkRenderer
   def to_html
     html = ''
-    html += tag("a", "Submit and Go to next comparison", :href => url(current_page + 1), :class => 'button large radius success') if current_page < total_pages
+    html += tag("a", "Top", :href => url(current_page + 1), :class => 'button large radius success') if current_page < total_pages
+    html += tag("a", "Bottom", :href => url(current_page + 1), :class => 'button large radius success') if current_page < total_pages
     html
   end
 end
