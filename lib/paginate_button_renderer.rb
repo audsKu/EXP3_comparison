9d324bc354746b2f78dd0291bd1994ec903236fa
audrey.kuo <audrey.kuo@gmail.com>
2013-06-08 09:53:59 -0400

    Dress up Next Button in pretty Zurb dresses.

--- lib/paginate_button_renderer.rb
+++ lib/paginate_button_renderer.rb
@@ -3,7 +3,7 @@
 class PaginateButtonRenderer < WillPaginate::ActionView::LinkRenderer
   def to_html
     html = ''
-    html += tag("a", "Submit and Go to next comparison", :href => url(current_page + 1)) if current_page < total_pages
+    html += tag("a", "Submit and Go to next comparison", :href => url(current_page + 1), :class => 'button large radius success') if current_page < total_pages
     html
   end
 end
